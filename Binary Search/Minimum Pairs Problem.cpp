878f2a33d33bdb5a051af5bcbadaa2cd13b18dd1
Harsh <hjain5164@gmail.com>
2021-07-10 21:13:46 +0530

    Solved the minimum pair problem using Binary Search

---
+++ Binary Search/Minimum Pairs Problem.cpp
@@ -0,0 +1,32 @@
+// Given tow non-empty arrays. Find the pair of numbers one from each 
+// arary whose absolute difference is minimum. Print the any one pair with
+// the smallest difference.
+#include <bits/stdc++.h>
+using namespace std;
+
+void min_pair(vector<int> a1, vector<int> a2){
+    sort(a2.begin(), a2.end());
+    int p1, p2;
+    int diff = INT_MAX;
+    for(int x: a1){
+        auto lb = lower_bound(a2.begin(), a2.end(), x) - a2.begin();
+        
+        if(lb >= 1 && x - a2[lb - 1] < diff){
+            diff = x - a2[lb - 1];
+            p2 = x;
+            p1 = a2[lb - 1];
+        }
+        if(lb != a2.size() && a2[lb] - x < diff){
+            diff = a2[lb] - x;
+            p1 = x;
+            p2 = a2[lb];
+        }
+    }
+    cout << "Min Pair: " << p1 << ", " << p2;
+}
+
+int main() {
+    vector<int> a1 = {-1, 5, 10, 20, 3};
+    vector<int> a2 = {26,134,135,15,17};
+    min_pair(a1,a2);
+}
