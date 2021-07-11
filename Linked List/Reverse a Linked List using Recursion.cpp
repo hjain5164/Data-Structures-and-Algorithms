d3019fa7c34ee3af08d7a61688006f566c50d10d
Harsh <hjain5164@gmail.com>
2021-07-11 21:06:27 +0530

    Solved the problem to reverse a linked list

---
+++ Linked List/Reverse a Linked List using Recursion.cpp
@@ -0,0 +1,59 @@
+// Linked List - Given a Linked List. Write a function to reverse the linked list.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node * next;
+        node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+node* insertAtHead(node* head, int data){
+    if(head == NULL){
+        return new node(data);
+    }
+    node *temp = new node(data);
+    temp->next = head;
+    
+    return temp;
+}
+
+
+void printLinkedList(node *head){
+    while(head!= NULL){
+        cout << head -> data << "\n";
+        head = head->next;
+    }
+}
+
+node * reverse(node *head){
+    if(head == NULL || head -> next == NULL)
+        return head;
+
+    node* smallHead = reverse(head->next);
+    head->next->next = head;
+    head->next =NULL;
+    return smallHead;
+}
+
+int main() {
+    node* head = NULL;
+    head = insertAtHead(head, 7);
+    head = insertAtHead(head, 6);
+    head = insertAtHead(head, 5);
+    head = insertAtHead(head, 4);
+    head = insertAtHead(head, 3);
+    head = insertAtHead(head, 2);
+    head = insertAtHead(head, 1);
+
+    printLinkedList(head);
+    node * reverseHead = reverse(head);
+
+    printLinkedList(reverseHead);
+    
+}
